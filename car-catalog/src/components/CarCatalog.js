ac6392b5b4f0d04db780f67a8d2f8fe6678626cc
Your Name <abdullahi.abass@student.moringschool.com>
2023-08-06 18:12:02 +0300

    done

--- car-catalog/src/components/CarCatalog.js
+++ car-catalog/src/components/CarCatalog.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import CarCard from './CarCard';
 
-const CarCatalog = ({ cars, onCarSelect, onShuffleCars }) => {
+const CarCatalog = ({ cars, onCarSelect }) => {
   return (
     <div className="car-catalog">
       <h1 className="catalog-title">ABVSS Car Catalog</h1>
       <h2>These are the most famous cars belonging to Abvss car showroom.</h2>
-      {/* Add the Shuffle Cars button */}
-      <button className="shuffle-button" onClick={onShuffleCars}>
-        Shuffle Cars
-      </button>
       <div className="car-grid">
         {cars.map((car) => (
           <CarCard key={car.model} car={car} onSelect={() => onCarSelect(car)} />
