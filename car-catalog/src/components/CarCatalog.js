89ec2dbeaf73ff66631c9640b46d28b635ca8802
Your Name <abdullahi.abass@student.moringschool.com>
2023-08-06 18:36:32 +0300

    done

--- car-catalog/src/components/CarCatalog.js
+++ car-catalog/src/components/CarCatalog.js
@@ -5,7 +5,7 @@ const CarCatalog = ({ cars, onCarSelect }) => {
   return (
     <div className="car-catalog">
       <h1 className="catalog-title">ABVSS Car Catalog</h1>
-      <h2>These are the most famous cars belonging to Abvss car showroom.</h2>
+      <h2>These are the most famous cars in  Abvss car showroom.</h2>
       <div className="car-grid">
         {cars.map((car) => (
           <CarCard key={car.model} car={car} onSelect={() => onCarSelect(car)} />
