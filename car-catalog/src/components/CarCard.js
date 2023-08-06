6ed9678d6157ece2da7c4f9a99eccc3a4f058e81
Your Name <abdullahi.abass@student.moringschool.com>
2023-08-06 17:20:13 +0300

    like button added

--- car-catalog/src/components/CarCard.js
+++ car-catalog/src/components/CarCard.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const CarCard = ({ car, onSelect }) => {
+  const [likes, setLikes] = useState(0);
+
+  const handleLike = (event) => {
+    event.stopPropagation(); // Prevent the click event from bubbling up
+    setLikes((prevLikes) => prevLikes + 1);
+  };
+
   return (
     <div className="car-card" onClick={onSelect}>
       <img src={car.img_url} alt={car.model} className="car-image" />
@@ -8,6 +15,9 @@ const CarCard = ({ car, onSelect }) => {
         <h2>{car.brand}</h2>
         <h3>{car.model}</h3>
         <p>Price: {car.price}</p>
+        <button className="like-button" onClick={handleLike}>
+          Like ({likes})
+        </button>
       </div>
     </div>
   );
